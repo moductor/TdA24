7da2c3ac9aff6598b6b31df8759ef1fb9f16802c
Vojtěch Perník <develop@pervoj.cz>
2024-02-19 21:22:26 +0100

    Fix lecturer filtering API

--- src/database/functions/Lecturer.ts
+++ src/database/functions/Lecturer.ts
@@ -1,12 +1,13 @@
+import { Filter } from "mongodb";
 import {
   ContactInfo,
   Lecturer,
   LecturerBase,
   LecturerFilters,
   LecturerInput,
-  filter,
   getFilters as generateFilters,
 } from "../models/Lecturer";
+import { Pagination } from "../models/Pagination";
 import DB, { getUuid } from "./DB";
 import { get as getTag, getByName as getTagByName } from "./Tag";
 const db = DB.collection<LecturerDB>("lecturer");
@@ -79,16 +80,44 @@ export async function get(uuid: string): Promise<Lecturer | null> {
   return lecturer;
 }
 
-export type Pagination = {
-  skip: number;
-  limit: number;
-};
-
 export async function getAll(
   pagination?: Pagination,
   filters?: LecturerFilters,
 ): Promise<Lecturer[]> {
-  const cursor = db.find();
+  const dbFilter: Filter<LecturerDB> = {};
+
+  if (filters) {
+    dbFilter.price_per_hour = {
+      $gte: filters.price.value.min,
+      $lte: filters.price.value.max,
+    };
+
+    const selectedLocations = filters.location
+      .filter((e) => e.selected)
+      .map((e) => e.value);
+
+    if (selectedLocations.length) {
+      dbFilter.location = {
+        $in: selectedLocations,
+      };
+    }
+
+    const selectedTagUuids = (
+      await Promise.all(
+        filters.tags
+          .filter((e) => e.selected)
+          .map((e) => getTagByName(e.value)),
+      )
+    ).map((e) => e.uuid);
+
+    if (selectedTagUuids.length) {
+      dbFilter.tags = {
+        $all: selectedTagUuids,
+      };
+    }
+  }
+
+  const cursor = db.find(dbFilter);
 
   if (pagination) {
     cursor.skip(pagination.skip);
@@ -117,10 +146,6 @@ export async function getAll(
       return lecturer;
     }),
   );
-
-  if (filters) {
-    return filter(lecturers, filters);
-  }
 
   return lecturers;
 }
