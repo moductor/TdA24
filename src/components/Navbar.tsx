11147bddf1cd70c9409a453f4844ae658436433e
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 14:17:12 +0100

    Add user menu to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,42 @@
 import { styleClasses } from "../helpers/styleClasses";
 import styles from "./Navbar.module.scss";
 import Logo from "./widgets/Logo";
+import UserDropdownWrapper from "./widgets/UserDropdownWrapper";
 
 type Props = Readonly<{
   showBackButton?: boolean;
+  showUserMenu?: boolean;
   className?: string;
   [prop: string]: any;
 }>;
 
 export default function Navbar({
   showBackButton = false,
+  showUserMenu = true,
   className,
   ...props
 }: Props) {
   return (
     <nav {...props} className={styleClasses(styles, "navbar", className || "")}>
-      <a href="/" title="Přejít na domovskou stránku">
+      <a
+        href="/"
+        title="Přejít na domovskou stránku"
+        className={styleClasses(styles, "logo-link")}
+      >
         <Logo className={styleClasses(styles, "logo")} aria-hidden="true" />
       </a>
-      {showBackButton && (
-        <a
-          href="/"
-          aria-hidden="true"
-          className={styleClasses(styles, "menu-button")}
-        >
-          Domů
-        </a>
-      )}
+      <div className={styleClasses(styles, "menu-wrapper")}>
+        {showBackButton && (
+          <a
+            href="/"
+            aria-hidden="true"
+            className={styleClasses(styles, "menu-button")}
+          >
+            Domů
+          </a>
+        )}
+        {showUserMenu && <UserDropdownWrapper />}
+      </div>
     </nav>
   );
 }
