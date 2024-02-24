c5e27cb276c9f050921064b00d8289a11c30dbad
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 16:05:41 +0100

    Update ProfilePicture component

--- src/components/widgets/ProfilePicture.tsx
+++ src/components/widgets/ProfilePicture.tsx
@@ -4,16 +4,17 @@ import styles from "./ProfilePicture.module.scss";
 
 type Props = {
   src?: string;
+  className?: string;
 };
 
-export default function ProfilePicture({ src }: Props) {
+export default function ProfilePicture({ src, className }: Props) {
   return (
     <Image
-      src={src || ""}
+      src={src || "/images/profile-placeholder.svg"}
       alt=""
       width={512}
       height={512}
-      className={styleClasses(styles, "picture")}
+      className={styleClasses(styles, "picture", className || "")}
     />
   );
 }
