0c0f844938bc45816c538ecb752141597afd2695
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 14:02:20 +0100

    Add user login API

---
+++ src/app/api/user/register/RegisterQuery.ts
@@ -0,0 +1,5 @@
+export type RegisterQuery = {
+  email: string;
+  username: string;
+  password: string;
+};
---
+++ src/app/api/user/register/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { insertOne } from "../../../../database/functions/User";
+import { hash } from "../../../../helpers/passwordHash";
+import { RegisterQuery } from "./RegisterQuery";
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  const data = (await request.json()) as RegisterQuery;
+
+  const res = await insertOne({
+    username: data.username,
+    email: data.email,
+    passwordHash: hash(data.password),
+  });
+
+  if (typeof res != "string") {
+    return NextResponse.json(res, { status: 409 });
+  }
+
+  return NextResponse.json(res, { status: 201 });
+}
