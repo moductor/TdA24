66315767bf69a04f2dc605bb9ae95e0faf1fc0af
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 19:51:56 +0100

    Make Header component more univarsal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Header from "../components/Header";
 import HomeView from "../components/partials/home/HomeView";
 import { loadCount } from "../components/partials/home/lecturerFetcher";
 import { getAll, getFilters } from "../database/functions/Lecturer";
+import { styleClasses } from "../helpers/styleClasses";
+import styles from "./page.module.scss";
 
 export default async function Page() {
   const lecturers = await getAll({ skip: 0, limit: loadCount });
@@ -10,7 +12,16 @@ export default async function Page() {
 
   return (
     <>
-      <Header />
+      <Header>
+        <div className={styleClasses(styles, "hero")}>
+          <h1 className={styleClasses(styles, "subtitle")}>
+            Dynamický katalog lektorů
+          </h1>
+          <p className={styleClasses(styles, "title")}>
+            Vaše <span>vzdělání</span> je u nás v dobrých rukou!
+          </p>
+        </div>
+      </Header>
 
       <main className="content-grid" style={{ marginBlock: "4rem" }}>
         <HomeView
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,21 @@
+import { ReactNode } from "react";
 import { styleClasses } from "../helpers/styleClasses";
 import styles from "./Header.module.scss";
 import Navbar from "./Navbar";
 import BackgroundWrapper from "./widgets/BackgroundWrapper";
 
 type Props = Readonly<{
+  children?: ReactNode;
   [prop: string]: any;
 }>;
 
-export default function Header(props: Props) {
+export default function Header({ children, ...props }: Props) {
   return (
     <header {...props} className={styleClasses(styles, "header")}>
       <BackgroundWrapper>
-        <div className={styleClasses(styles, "content-grid")}>
+        <div className="content-grid">
           <Navbar />
-          <div className={styleClasses(styles, "content")}>
-            <h1 className={styleClasses(styles, "subtitle")}>
-              Dynamický katalog lektorů
-            </h1>
-            <p className={styleClasses(styles, "title")}>
-              Vaše <span>vzdělání</span> je u nás v dobrých rukou!
-            </p>
-          </div>
+          <div>{children}</div>
         </div>
       </BackgroundWrapper>
     </header>
