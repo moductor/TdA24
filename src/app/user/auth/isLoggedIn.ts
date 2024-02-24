1ef00f7abcae461cc2eeccb12dbf606c5d754e8f
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 14:08:36 +0100

    Add session cookie checker

---
+++ src/app/user/auth/isLoggedIn.ts
@@ -0,0 +1,20 @@
+import { Jwt } from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { User } from "../../../database/models/User";
+import { isTokenValid, parseUserToken } from "../../../helpers/tokenParser";
+
+export function getUserTokenWithSession(): Jwt | undefined {
+  const tokenCookie = cookies().get("JWT")?.value;
+  return parseUserToken(tokenCookie);
+}
+
+export function isLoggedInWithSession(token?: Jwt): boolean {
+  if (!token) token = getUserTokenWithSession();
+  return isTokenValid(token);
+}
+
+export function getCurrentUserWithSession(): User | undefined {
+  const token = getUserTokenWithSession();
+  if (!isLoggedInWithSession(token)) return;
+  return token!.payload as User;
+}
