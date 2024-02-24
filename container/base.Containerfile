a3b9c67aed1bddd89938f742c6066689632958c1
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 17:23:33 +0100

    Split Docker images

---
+++ container/base.Containerfile
@@ -0,0 +1,6 @@
+FROM node:current-alpine
+
+ENV NEXT_TELEMETRY_DISABLED=1
+
+# https://github.com/vercel/next.js/blob/canary/examples/with-docker/Dockerfile#L5
+RUN apk add --no-cache libc6-compat;
--- container/build.sh
+++ container/build.sh
@@ -3,4 +3,5 @@
 _CURRENT_DIR=$(dirname "$0")
 source $_CURRENT_DIR/_vars.sh
 
+$PODMAN build $PROJECT_DIR -f $CONTAINER_DIR/base.Containerfile -t localhost/base
 $PODMAN build $PROJECT_DIR -f $CONTAINER_DIR/Containerfile -t $IMAGE
