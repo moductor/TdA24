ec4d7f68993357be7d6dd8f42a0054bd7838d13f
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 17:32:07 +0100

    Add Docker Compose for running locally

--- container/run.sh
+++ container/run.sh
@@ -5,17 +5,4 @@ _CONTAINER="docker.io/library/mongo:latest"
 _CURRENT_DIR=$(dirname "$0")
 source $_CURRENT_DIR/_vars.sh
 
-DB_CONTAINER_ID_FILE="$PROJECT_DIR/_container_id_db.tmp"
-
-if [[ -f "$DB_CONTAINER_ID_FILE" ]]; then
-  DB_CONTAINER_ID=$(cat "$DB_CONTAINER_ID_FILE")
-fi
-
-if [[ -z "$DB_CONTAINER_ID" ]]; then
-  DB_CONTAINER_ID=$($PODMAN container create -p $MONGODB_PORT:27017 $_CONTAINER)
-  echo "$DB_CONTAINER_ID" > $DB_CONTAINER_ID_FILE
-fi
-
-$PODMAN container start $DB_CONTAINER_ID
-npm install
-npm run dev
+(cd $CONTAINER_DIR; $PODMAN compose -f docker-compose.yml up --no-attach mongodb)
