4a90c27bd76c599ac4cd1c5e5fe2842f86f48d2e
Vojtěch Perník <develop@pervoj.cz>
2024-02-24 19:54:46 +0100

    Add way to control navbar via header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,23 @@ import Navbar from "./Navbar";
 import BackgroundWrapper from "./widgets/BackgroundWrapper";
 
 type Props = Readonly<{
+  showBackButton?: boolean;
+  showUserMenu?: boolean;
   children?: ReactNode;
   [prop: string]: any;
 }>;
 
-export default function Header({ children, ...props }: Props) {
+export default function Header({
+  showBackButton,
+  showUserMenu,
+  children,
+  ...props
+}: Props) {
   return (
     <header {...props} className={styleClasses(styles, "header")}>
       <BackgroundWrapper>
         <div className="content-grid">
-          <Navbar />
+          <Navbar showBackButton={showBackButton} showUserMenu={showUserMenu} />
           <div>{children}</div>
         </div>
       </BackgroundWrapper>
