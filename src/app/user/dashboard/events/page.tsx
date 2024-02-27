407c7e6d0d9cea474d6337020542824a67a19fbc
Dominik <dominikbartuska@gmail.com>
2024-02-27 22:10:51 +0100

    pridani kalendare do dashboard

--- src/app/user/dashboard/events/page.tsx
+++ src/app/user/dashboard/events/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <></>;
+import Calendar from "../../../../components/widgets/Calendar";
+import DashboardSection from "../DashboardSection";
+import PageHeader from "../PageHeader";
+
+import { get } from "../../../../database/functions/Lecturer";
+import { getCurrentUserWithSession } from "../../../../helpers/userContext";
+
+export default async function Page() {
+  let isLecturer = false;
+
+  const user = getCurrentUserWithSession();
+  if (user?.lecturerId) {
+    const lecturer = await get(user.lecturerId);
+    if (lecturer) {
+      //const lecturerStr = JSON.stringify(lecturer);
+      isLecturer = true;
+    }
+  }
+
+  const events = [
+    { title: "event 1", date: "2024-02-01" },
+    { title: "event 2", date: "2024-02-22" },
+  ];
+
+  return (
+    <>
+      <PageHeader title="Rezervované schůzky" />
+
+      <DashboardSection title="Kalendář schůzek">
+        <Calendar isLecturer={isLecturer} events={events} />
+      </DashboardSection>
+    </>
+  );
 }
