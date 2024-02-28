96088cf557fa37d5aff94a97cf63d6dcb20c0e69
Vojtěch Perník <develop@pervoj.cz>
2024-02-28 17:45:18 +0100

    Add way to add class to result indicator

--- src/components/widgets/ResultIndicator.tsx
+++ src/components/widgets/ResultIndicator.tsx
@@ -4,16 +4,17 @@ import styles from "./ResultIndicator.module.scss";
 
 type Props = {
   result?: boolean | null;
+  className?: string;
 };
 
-export default function ResultIndicator({ result }: Props) {
+export default function ResultIndicator({ result, className }: Props) {
   if (result === undefined) return <></>;
 
   if (result === null) return <LoadingBar />;
 
   return (
     <span
-      className={styleClasses(styles, "result")}
+      className={styleClasses(styles, "result", className || "")}
       data-result={result}
     ></span>
   );
