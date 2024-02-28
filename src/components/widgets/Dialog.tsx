f8d1c05bcb93509b9788863d019c36839f15b899
Dominik <dominikbartuska@gmail.com>
2024-02-28 20:36:33 +0100

    fix dialog component

--- src/components/widgets/Dialog.tsx
+++ src/components/widgets/Dialog.tsx
@@ -2,30 +2,15 @@ import { useEffect, useRef } from "react";
 import { styleClasses } from "../../helpers/styleClasses";
 import Card from "./Card";
 import styles from "./Dialog.module.scss";
-import Button from "./forms/Button";
 
 type Props = Readonly<{
   show: boolean;
-  text: string;
-  hideDialogModal: any;
-  onAccept: any;
-  onDecline?: any;
-  acceptBtnText?: string;
-  declienBtnText?: string;
   className?: string;
+  children: React.ReactNode;
   [prop: string]: any;
 }>;
 
-export default function Dialog({
-  show = false,
-  text,
-  acceptBtnText = "Ano",
-  declienBtnText = "Ne",
-  hideDialogModal,
-  onAccept,
-  className,
-  ...props
-}: Props) {
+export default function Dialog({ show, className, children, ...props }: Props) {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
@@ -43,29 +28,7 @@ export default function Dialog({
           {...props}
         >
           <Card className={styleClasses(styles, "dialog-card")}>
-            <div className={styleClasses(styles, "dialog-text")}>{text}</div>
-            <div className={styleClasses(styles, "dialog-btns")}>
-              <Button
-                variant="destructive"
-                onClick={async () => {
-                  onAccept();
-                  hideDialogModal();
-                  document.querySelector("body")!.style.overflow = "";
-                  dialogRef.current?.close();
-                }}
-              >
-                {acceptBtnText}
-              </Button>
-              <Button
-                onClick={() => {
-                  hideDialogModal();
-                  document.querySelector("body")!.style.overflow = "";
-                  dialogRef.current?.close();
-                }}
-              >
-                {declienBtnText}
-              </Button>
-            </div>
+            {children}
           </Card>
         </dialog>
       )}
