213fa8801a458dc37eb229017216594e90e2be20
Dominik <dominikbartuska@gmail.com>
2024-03-01 20:56:24 +0100

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { styleClasses } from "../helpers/styleClasses";
 import styles from "./Footer.module.scss";
+import Logo from "./widgets/Logo";
 
 export default function Footer() {
   const date = new Date();
@@ -7,7 +9,28 @@ export default function Footer() {
 
   return (
     <footer className={styleClasses(styles, "footer")}>
-      &copy; {year} Teacher digital Agency
+      <div className={styleClasses(styles, "footer-info")}>
+        <div>
+          <Link
+            href="/"
+            title="Přejít na domovskou stránku"
+            className={styleClasses(styles, "logo-link")}
+          >
+            <Logo className={styleClasses(styles, "logo")} aria-hidden="true" />
+          </Link>
+          <span className={styleClasses(styles, "copyright")}>
+            &copy; {year} Teacher digital Agency
+          </span>
+        </div>
+        <ul className={styleClasses(styles, "footer-list")}>
+          <li>
+            <Link href="/gdpr">Zásady ochrany osobních údajů</Link>
+          </li>
+          <li>
+            <Link href="/tos">Podmínky používání</Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
