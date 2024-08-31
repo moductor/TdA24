784d4f72163f17f97892838db4221c1f8a495815
Dominik Bartuška <dominikbartuska@gmail.com>
2024-08-31 14:57:31 +0200

    translate registration

--- src/components/partials/user/login/RegisterForm.tsx
+++ src/components/partials/user/login/RegisterForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { useState } from "react";
 import { RegisterQuery } from "../../../../app/api/user/register/RegisterQuery";
@@ -16,6 +17,8 @@ export default function RegisterForm() {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>();
 
+  const t = useTranslations("Auth.register");
+
   async function onSubmit(data: FormData) {
     setIsLoading(true);
 
@@ -29,15 +32,13 @@ export default function RegisterForm() {
 
     if (!regularExpression.test(password)) {
       setIsLoading(false);
-      setErrorMessage(
-        "Heslo musí obsahovat nejméně 8 znaků, velká a malá písmena, alespoň 1 číslo a speciální znak!",
-      );
+      setErrorMessage(t("passwordError"));
       return;
     }
 
     if (password !== passwordCheck) {
       setIsLoading(false);
-      setErrorMessage("Hesla se neshodují!");
+      setErrorMessage(t("passwordMatchError"));
       return;
     }
 
@@ -60,27 +61,27 @@ export default function RegisterForm() {
       const conflict = resData.conflict;
 
       if (conflict?.username && conflict?.email) {
-        setErrorMessage("Zadané uživatelské jméno a email už existují.");
+        setErrorMessage(t("conflictUsernameEmailError"));
         return;
       }
 
       if (conflict?.username) {
-        setErrorMessage("Zadané uživatelské jméno už existuje.");
+        setErrorMessage(t("conflictUsernameError"));
         return;
       }
 
       if (conflict?.email) {
-        setErrorMessage("Zadaný email už existuje.");
+        setErrorMessage(t("conflictEmailError"));
         return;
       }
     }
 
-    setErrorMessage("Při registraci došlo k chybě.");
+    setErrorMessage(t("generalError"));
   }
 
   return (
     <form action={onSubmit} className={styleClasses(styles, "form")}>
-      <h1 className={styleClasses(styles, "title", "title-2")}>Registrace</h1>
+      <h1 className={styleClasses(styles, "title", "title-2")}>{t("title")}</h1>
 
       {errorMessage && (
         <div className={styleClasses(styles, "error-box")}>{errorMessage}</div>
@@ -92,7 +93,7 @@ export default function RegisterForm() {
         required={true}
         className={styleClasses(styles, "form-field")}
       >
-        Uživatelské jméno
+        {t("username")}
       </TextFieldRow>
 
       <TextFieldRow
@@ -101,7 +102,7 @@ export default function RegisterForm() {
         required={true}
         className={styleClasses(styles, "form-field")}
       >
-        Email
+        {t("email")}
       </TextFieldRow>
 
       <TextFieldRow
@@ -110,7 +111,7 @@ export default function RegisterForm() {
         required={true}
         className={styleClasses(styles, "form-field")}
       >
-        Heslo
+        {t("password")}
       </TextFieldRow>
 
       <TextFieldRow
@@ -119,17 +120,17 @@ export default function RegisterForm() {
         required={true}
         className={styleClasses(styles, "form-field")}
       >
-        Kontrola hesla
+        {t("passwordCheck")}
       </TextFieldRow>
 
       <div className={styleClasses(styles, "submit-row", "form-field")}>
-        <Button>Zaregistrovat se</Button>
+        <Button>{t("registerButton")}</Button>
       </div>
 
       <p className={styleClasses(styles, "footer")}>
-        Již máte účet?{" "}
+        {t("footer")}{" "}
         <Link href="/user/auth/login">
-          <Tag className="text-jet background-sky-blue">Přihlaste se!</Tag>
+          <Tag className="text-jet background-sky-blue">{t("loginLink")}</Tag>
         </Link>
       </p>
 
