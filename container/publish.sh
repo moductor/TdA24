aae28d7977bc983cdd51b63c39777740386ffb5d
Vojtěch Perník <develop@pervoj.cz>
2026-01-06 10:11:59 +0100

    ci: publish to ghcr

--- container/publish.sh
+++ container/publish.sh
@@ -3,16 +3,9 @@
 _CURRENT_DIR=$(dirname "$0")
 source $_CURRENT_DIR/_vars.sh
 
-SECRET=${SECRET:-""}
-CREDENTIALS_URL="https://auth.registry.tourdeapp.eu/staging-auth"
-REGISTRY_URL="registry.tourdeapp.eu"
-REGISTRY_USERNAME="tourdeapp"
+IMAGE_REGISTRY=${IMAGE_REGISTRY:-""}
+REGISTRY_USER=${REGISTRY_USER:-""}
+REGISTRY_PASSWORD=${REGISTRY_PASSWORD:-""}
 
-_RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"team_secret\": \"$SECRET\"}" $CREDENTIALS_URL)
-
-REGISTRY_PASSWORD=$(echo $_RESPONSE | jq -r .key)
-REMOTE_IMAGE=$(echo $_RESPONSE | jq -r .name)
-
-$PODMAN login --username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD $REGISTRY_URL
-$PODMAN tag $IMAGE $REMOTE_IMAGE
-$PODMAN push $REMOTE_IMAGE
+$PODMAN login --username $REGISTRY_USER --password $REGISTRY_PASSWORD $IMAGE_REGISTRY
+$PODMAN push $IMAGE $IMAGE_REGISTRY/$IMAGE
