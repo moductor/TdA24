073f6f9cb6e4170f59ef36ecf709834e3e1ad037
Vojtěch Perník <develop@pervoj.cz>
2026-01-06 15:48:49 +0100

    ci: delete github actions

--- container/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-_CURRENT_DIR=$(dirname "$0")
-source $_CURRENT_DIR/_vars.sh
-
-$PODMAN build $PROJECT_DIR -f $CONTAINER_DIR/base.Containerfile -t localhost/base
-$PODMAN build $PROJECT_DIR -f $CONTAINER_DIR/Containerfile -t $IMAGE
--- container/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-_CONTAINER="docker.io/library/mongo:latest"
-
-_CURRENT_DIR=$(dirname "$0")
-source $_CURRENT_DIR/_vars.sh
-
-(cd $CONTAINER_DIR; $PODMAN compose -f docker-compose.yml up --no-attach mongodb)
