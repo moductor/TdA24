690fa460e97a5f07f8a3a9039fc7bcb15b3fa86c
Vojtěch Perník <develop@pervoj.cz>
2026-01-06 11:06:42 +0100

    ci: fix dynamic build

--- src/app/api/generate-placeholder-data/route.ts
+++ src/app/api/generate-placeholder-data/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import createPlaceholderData from "../../../database/placeholderData";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(): Promise<NextResponse> {
   await createPlaceholderData(true);
   return new NextResponse("generated", { status: 200 });
