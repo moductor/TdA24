d7b50c467ea38ecd7e5ee5c3b2d12d9efd8a53c7
Vojtěch Perník <develop@pervoj.cz>
2026-01-06 16:01:38 +0100

    fix: build errors

--- src/helpers/userContext.ts
+++ src/helpers/userContext.ts
@@ -1,20 +1,28 @@
 import { Jwt } from "jsonwebtoken";
-import { cookies } from "next/headers";
+import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 import { User } from "../database/models/User";
 import { isTokenValid, parseUserToken } from "./tokenParser";
 
-export async function getUserTokenWithSession(): Promise<Jwt | undefined> {
-  const tokenCookie = (await cookies()).get("JWT")?.value;
+export async function getUserTokenWithSession(
+  currentCookies?: ReadonlyRequestCookies,
+): Promise<Jwt | undefined> {
+  const c = currentCookies ?? (await require("next/headers").cookies());
+  const tokenCookie = c.get("JWT")?.value;
   return parseUserToken(tokenCookie);
 }
 
-export async function isLoggedInWithSession(token?: Jwt): Promise<boolean> {
-  if (!token) token = await getUserTokenWithSession();
+export async function isLoggedInWithSession(
+  token?: Jwt,
+  cookies?: ReadonlyRequestCookies,
+): Promise<boolean> {
+  if (!token) token = await getUserTokenWithSession(cookies);
   return isTokenValid(token);
 }
 
-export async function getCurrentUserWithSession(): Promise<User | undefined> {
-  const token = await getUserTokenWithSession();
+export async function getCurrentUserWithSession(
+  cookies?: ReadonlyRequestCookies,
+): Promise<User | undefined> {
+  const token = await getUserTokenWithSession(cookies);
   if (!token || !isLoggedInWithSession(token)) return;
   return token!.payload as User;
 }
